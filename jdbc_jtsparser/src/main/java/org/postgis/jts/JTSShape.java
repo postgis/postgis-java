7db13238085ba73498bea30847d3539165ab3f05
Phillip Ross <phillip.w.g.ross@gmail.com>
2015-11-08 22:06:37 -0500

    Addresses https://github.com/postgis/postgis-java/issues/35 - Modifications to source headers to use proper GPL 2.1+ license template.

--- jdbc_jtsparser/src/main/java/org/postgis/jts/JTSShape.java
+++ jdbc_jtsparser/src/main/java/org/postgis/jts/JTSShape.java
@@ -1,3 +1,28 @@
+/*
+ * JTSShape.java
+ *
+ * Binary Parser for JTS - relies on org.postgis V1.0.0+ package.
+ *
+ * (C) 2005 Markus Schaber, markus.schaber@logix-tt.com
+ *
+ * (C) 2015 Phillip Ross, phillip.w.g.ross@gmail.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
 package org.postgis.jts;
 
 import java.awt.Rectangle;
