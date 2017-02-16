08288195801057932a188467c3c7894ca310a17d
Phillip Ross <phillip.w.g.ross@gmail.com>
2017-02-16 13:12:11 -0500

    added copyright header to new serialization test class and incorporated into surefire

--- jdbc/src/test/java/org/postgis/SerializationTest.java
+++ jdbc/src/test/java/org/postgis/SerializationTest.java
@@ -1,4 +1,32 @@
+/*
+ * ServerTest.java
+ *
+ * PostGIS extension for PostgreSQL JDBC driver - example and test classes
+ *
+ * (C) 2004 Paul Ramsey, pramsey@refractions.net
+ *
+ * (C) 2005 Markus Schaber, markus.schaber@logix-tt.com
+ *
+ * (C) 2017 Phillip Ross, phillip.w.g.ross@gmail.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
 package org.postgis;
+
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -7,7 +35,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.NotSerializableException;
 import java.io.ObjectOutputStream;
 
+
 public class SerializationTest {
+
 
     @Test
     public void serializationCheckPGgeometry() throws Exception {
@@ -19,4 +49,6 @@ public class SerializationTest {
             Assert.fail("serialization of PGgeometry failed: " + ex);
         }
     }
+
+
 }
