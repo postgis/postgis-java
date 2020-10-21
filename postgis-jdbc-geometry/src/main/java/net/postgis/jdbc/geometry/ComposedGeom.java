93c44fc7197a82e398aeb3940c7539944c8731e0
Tobias <tobias@olsson.be>
2020-10-21 18:58:52 +0200

    Make e.g. new MultiPolygon().getPolygons() work
    Since there is an explicit cast in e.g. MultiPolygon, it is only correct
    for GeometryCollection to have subgeoms as an Geometry[] instance. All
    other geometries will throw ClassCastException in getDistance()

--- postgis-jdbc-geometry/src/main/java/net/postgis/jdbc/geometry/ComposedGeom.java
+++ postgis-jdbc-geometry/src/main/java/net/postgis/jdbc/geometry/ComposedGeom.java
@@ -45,14 +45,12 @@ public abstract class ComposedGeom extends Geometry {
     /* JDK 1.5 Serialization */
     private static final long serialVersionUID = 0x100;
 
-    public static final Geometry[] EMPTY = new Geometry[0];
-
     /**
      * The Array containing the geometries
      * 
      * This is only to be exposed by concrete subclasses, to retain type safety.
      */
-    protected Geometry[] subgeoms = EMPTY;
+    protected Geometry[] subgeoms = createSubGeomArray(0);
 
     /**
      * Constructs an instance with the specified type
