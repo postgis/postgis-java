9587343e74aca087dcf8220e1d0c248ed7cc723e
Phillip Ross <phillip.w.g.ross@gmail.com>
2025-03-03 05:43:06 -0500

    Build Updates - CI updated to zulu v8.0.442/v11.0.26/v17.0.4/v21.0.6 - removed maven install/config from GHA workflow and associated shell script - switch GHA from maven to openjdk - updated CI commands to invoke maven w/ mvnw instead of mvn - added .sdkmanrc with initial settings for java as v8.0.442-zulu - added maven-wrapper-plugin v3.3.2 - introduced maven wrapper w/ maven version set to v3.3.9 - add blank .mvn/jvm.config as placeholder - add .mvn/maven.config with checksum/snapshot options and javadoc path to java8 javadoc - updated minimum maven version from v3.8 to v3.9 - download-maven-plugin updated from v1.9.0 to v1.13.0 - exec-maven-plugin updated from v3.3.0 to v3.5.0 - maven-archetype-plugin updated from v3.2.1 to v3.3.1 - maven-checkstyle-plugin updated from v3.4.0 to v3.6.0 - maven-clean-plugin updated from v3.4.0 to v3.4.1 - maven-compiler-plugin updated from v3.13.0 to v3.14.0 - maven-dependency-plugin updated from v3.7.1 to v3.8.1 - maven-deploy-plugin updated from v3.1.2 to v3.1.4 - maven-failsafe-plugin updated from v3.3.1 to v3.5.2 - maven-gpg-plugin updated from v3.2.4 to v3.2.7 - maven-install-plugin updated from v3.1.2 to v3.1.4 - maven-jarsigner-plugin updated from v3.0.0 to v3.1.0 - maven-javadoc-plugin updated from v3.8.0 to v3.11.2 - maven-jxr-plugin updated from v3.4.0 to v3.6.0 - maven-pmd-plugin updated from v3.24.0 to v3.26.0 - maven-project-info-reports-plugin updated from v3.6.2 to v3.9.0 - maven-site-plugin updated from v3.12.1 to v3.21.0 - maven-surefire-plugin updated from v3.3.1 to v3.5.2 - maven-surefire-report-plugin updated from v3.3.1 to v3.5.2 - spotbugs-maven-plugin updated from v4.8.6.2 to v4.8.6.6 - versions-maven-plugin updated from v2.17.1 to v2.18.0 - jts-version updated from v1.19.0 to v1.20.0 - logback updated from v1.3.14 to v1.3.15 - pmd updated from v7.4.0 to v7.11.0 - postgresql-jdbc updated from v42.7.3 to v42.7.5 - slf4j updated from v2.0.13 to v2.0.17 - testcontainers updated from v1.20.1 to v1.20.5 - maven-version-rules.xml updated to include latest rules for ignore dependency versions requiring later versions of Java than v8
    Signed-off-by: Phillip Ross <phillip.w.g.ross@gmail.com>

--- .github/install-sdkman.sh
+++ .github/install-sdkman.sh
@@ -3,7 +3,6 @@
 set -euf -o pipefail
 
 JAVA_VERSION=$1
-MAVEN_VERSION=$2
 
 printf "Installing, configuring, and initializing SDKMAN\n"
 curl -s "https://get.sdkman.io" | bash
@@ -12,7 +11,4 @@ set +uf
 source "${HOME}/.sdkman/bin/sdkman-init.sh"
 
 printf "Installing Azul Zulu JDKs via SDKMAN\n"
-sdk i java ${JAVA_VERSION}
-
-printf "Installing Apache Maven via SDKMAN\n"
-sdk i maven ${MAVEN_VERSION}
+sdk i java "${JAVA_VERSION}"
